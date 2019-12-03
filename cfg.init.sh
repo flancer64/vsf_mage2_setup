c6e97dd7474fa7efae5d334ce4ab5c8c32ff5961
Alex Gusev <alex@flancer64.com>
2019-12-03 22:49:29 +0200

    split scripts by steps

--- cfg.init.sh
+++ cfg.init.sh
@@ -6,8 +6,8 @@
 export HOST_VSF="255.255.255.255"   # ip address or domain name for VSF host (VSF Front/API, Elasticsearch & Redis)
 export HOST_MAGE="mage2.host.com"   # ip address or domain name for Magento 2 host
 # address of REST API of source Magento instance
-export URL_MAGE_REST="http://${URL_MAGE}/rest"
-export URL_MAGE_IMG="http://${URL_MAGE}/media/catalog/product"
+export URL_MAGE_REST="http://${HOST_MAGE}/rest"
+export URL_MAGE_IMG="http://${HOST_MAGE}/media/catalog/product"
 export INDEX_NAME="vue_storefront_catalog"
 # Magento integration options
 # see: "How to integrate Magento2 with your local instance?"
